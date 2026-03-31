c7458dd1b0199e61c1f612dc5292c2ec3d89c8f1
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2026-03-31 10:55:51 -0400

    binki-atlassian-jira-rovodev-dismiss-1.0.1

--- binki-atlassian-jira-rovodev-dismiss.user.js
+++ binki-atlassian-jira-rovodev-dismiss.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name binki-atlassian-jira-rovodev-dismiss
-// @version 1.0.0
+// @version 1.0.1
 // @homepageURL https://github.com/binki/binki-atlassian-jira-rovodev-dismiss
 // @match https://*.atlassian.net/*
 // @require https://github.com/binki/binki-userscript-delay-async/raw/252c301cdbd21eb41fa0227c49cd53dc5a6d1e58/binki-userscript-delay-async.js
